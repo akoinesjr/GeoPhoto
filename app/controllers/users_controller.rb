c768636725be56c6cb0ecd3575788f2be89d7aea
Andrew <akoines@zoho.com>
2014-10-09 15:32:13 -0700

    added pages, users, photos, user sign up in progress

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  def show
+  end
+
+end
